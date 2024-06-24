2420848ddafe2d42cb56ec94d91a00e988a8371c
Jay Rajput <jayrajput512199@gmail.com>
2024-06-25 01:03:08 +0530

    create model

--- djangorestapi/restapi/models.py
+++ djangorestapi/restapi/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Userdetails(models.Models):
+    user_name = models.CharField(max_length=64)
+    user_age = models.IntegerField()
+    MY_CHOICES = (
+        ('a', 'Male'),
+        ('b', 'Female'),
+        ('c', 'Other'),
+    )
+    user_gender = models.CharField(max_length=20,choice= MY_CHOICES,default='a' )
+    user_scoolname = models.CharField(max_length=1024
+    ) 
--- djangorestapi/restapi/views.py
+++ djangorestapi/restapi/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view # type: ignore
+from rest_framework.response import Response # type: ignore
 
 # Create your views here.
+
+
+def getuserdetails(request):
+    
+    return Response()
+
+
